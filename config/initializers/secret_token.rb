c95adb290e92519234e3fc50eb9bfe2f35f6095c
Pawel Kwiecien <p.kwiecien@tum.de>
2014-02-05 23:26:12 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tumitfahrer::Application.config.secret_key_base = 'bc632e1da23ea8551931c1ddffb0b559ba050ba9788f97160cd263c6d31a81cf24105344cfe173dfd1d0fa7b8b1a1eb9092090d382b498c5b4f9b5a363d6de87'
