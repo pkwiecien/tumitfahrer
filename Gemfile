b3f0ace2ff4b2e54b68e270f3ab585c674c59faf
Pawel Kwiecien <p.kwiecien@tum.de>
2014-02-07 08:06:44 +0100

    Finish adding static pages, add User controller

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.0.0'
 gem 'rails', '4.0.2'
+gem "bootstrap-sass", "~> 3.1.0.2"
 
 group :development, :production do
   gem 'pg', '0.15.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Tumitfahrer::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get "users/new"
+
+  root "static_pages#home"
+  match "signup", to: "users#new", via: 'get'
+  match "/help", to: "static_pages#help", via: 'get'
+  match "/contact", to: "static_pages#contact", via: 'get'
+  match "/about", to: "static_pages#about", via: 'get'
+  match "/rides", to: "static_pages#rides", via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
