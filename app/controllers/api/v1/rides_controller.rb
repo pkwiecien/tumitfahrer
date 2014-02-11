5eb905f8e71732bc27932986dc273adbfa519bb0
Pawel Kwiecien <p.kwiecien@tum.de>
2014-02-11 18:39:13 +0100

    API updated

--- app/controllers/api/v1/rides_controller.rb
+++ app/controllers/api/v1/rides_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RidesController < ApiController
   respond_to :json, :xml
-  before_action :restrict_access, only: [:index]
+  before_action :restrict_access, only: [:index, :create]
 
   def index
     @rides = Ride.all
@@ -19,6 +19,14 @@ class Api::V1::RidesController < ApiController
   end
 
   def create
+    current_user = User.find_by(api_key: request.headers['apiKey'])
+    logger.debug request.headers['apiKey']
+    @ride = current_user.rides.build(ride_params)
+    if @ride.save
+      render json: {:result => "1"}
+    else
+      render json: {:result => "0"}
+    end
 
   end
 
@@ -28,5 +36,9 @@ class Api::V1::RidesController < ApiController
     authenticate_or_request_with_http_token do |key, options|
       User.exists?(api_key: key)
     end
+  end
+
+  def ride_params
+    params.require(:ride).permit(:departure_place, :destination, :departure_time, :free_seats, :meeting_point)
   end
 end
