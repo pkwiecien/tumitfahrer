71aa7c0c1b8f0ac04baa174a4f66b69ab23bbbf7
Pawel Kwiecien <p.kwiecien@tum.de>
2014-03-18 18:19:10 +0100

    delete not needed files and add last methods to api

--- app/models/friend_request.rb
+++
@@ -1,4 +0,0 @@
-class FriendRequest < ActiveRecord::Base
-  belongs_to :from_user, class_name: "User"
-  belongs_to :to_user, class_name: "User"
-end
--- db/migrate/20140316090117_rename_friends_requests.rb
+++
@@ -1,5 +0,0 @@
-class RenameFriendsRequests < ActiveRecord::Migration
-  def change
-      rename_table :friend_requests, :friendship_requests
-  end
-end
