654b75624a750e4cc35e89534eadf72d0ef310f4
Pawel Kwiecien <p.kwiecien@tum.de>
2014-03-18 08:21:53 +0100

    add further api methods

--- app/serializers/legacy_user_serializer.rb
+++ app/serializers/legacy_user_serializer.rb
@@ -1,15 +1,6 @@
 class LegacyUserSerializer < ActiveModel::Serializer
-  [:id, :first_name, :last_name, :department, :car, :api_key, :phone_number, :ride_count, :exp, :ratings,
-   :unbound_contributions, :rank, :email, :is_student].each do |attr|
-    # Tell serializer its an attribute
-    attribute attr
-
-    # Define a method with the same name as the attribute that calls the
-    # underlying object and to_s on the result
-    define_method attr do
-      object.send(attr).to_s
-    end
-  end
+  attributes :id, :first_name, :last_name, :department, :car, :api_key, :phone_number, :ride_count, :exp, :ratings,
+   :unbound_contributions, :rank, :email, :is_student
 
   def ride_count
     object.rides.count
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,14 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  [:id, :first_name, :last_name, :department, :car, :api_key].each do |attr|
-    # Tell serializer its an attribute
-    attribute attr
-
-    # Define a method with the same name as the attribute that calls the
-    # underlying object and to_s on the result
-    define_method attr do
-      object.send(attr).to_s
-    end
-  end
+  attributes :id, :first_name, :last_name, :department, :car, :api_key
 
   def full_name
     "#{object.first_name} #{object.last_name}"
