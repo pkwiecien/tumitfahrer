93d35c583f097418a39d41911500b1c8f78a824e
Pawel Kwiecien <p.kwiecien@tum.de>
2014-04-13 17:45:12 +0200

    add further fields to ride serializer

--- app/serializers/ride_serializer.rb
+++ app/serializers/ride_serializer.rb
@@ -1,7 +1,7 @@
 class RideSerializer < ActiveModel::Serializer
   attributes :id, :departure_place, :destination, :meeting_point, :free_seats, :departure_time,
    :project_id, :price, :realtime_departure_time, :realtime_km, :driver_id, :requests, :passengers,
-   :contribution_mode, :is_paid, :duration, :pending_payments
+   :contribution_mode, :is_paid, :duration, :pending_payments, :distance, :created_at, :updated_at
 
   def project_id
     unless object.project.nil?
