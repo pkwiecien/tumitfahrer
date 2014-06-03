418e108ce77a85435a20e1d3088590f779bfb078
Pawel Kwiecien <p.kwiecien@tum.de>
2014-06-03 19:44:37 +0200

    delete old migrations

--- db/migrate/20140603165130_deletefieldsfromrelationship_and_request.rb
+++ db/migrate/20140603165130_deletefieldsfromrelationship_and_request.rb
@@ -1,6 +1,5 @@
 class DeletefieldsfromrelationshipAndRequest < ActiveRecord::Migration
   def change
-    remove_column :relationships, :driver_ride_id
     remove_column :requests, :requested_from
     remove_column :requests, :request_to
   end
