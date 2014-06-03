0412e7b89e01f19f8aefc661e6340854f0d784e0
Pawel Kwiecien <p.kwiecien@tum.de>
2014-06-03 19:43:07 +0200

    delete old migrations

--- db/migrate/20140603163316_deletefieldsfromuser.rb
+++
@@ -1,8 +0,0 @@
-class Deletefieldsfromuser < ActiveRecord::Migration
-  def change
-    remove_column :users, :rank
-    remove_column :users, :unbound_contributions
-    remove_column :users, :exp
-    remove_column :users, :gamification
-  end
-end
