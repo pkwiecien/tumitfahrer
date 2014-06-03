7607556899cf39f4aed610040ab1dae3e0102a4c
Pawel Kwiecien <p.kwiecien@tum.de>
2014-06-03 19:21:54 +0200

    remove uneeded migration

--- db/migrate/20140315152436_add_contribution_details_to_ride.rb
+++
@@ -1,6 +0,0 @@
-class AddContributionDetailsToRide < ActiveRecord::Migration
-  def change
-    add_column :rides, :contribution_mode, :integer
-    add_column :rides, :is_paid, :boolean
-  end
-end
