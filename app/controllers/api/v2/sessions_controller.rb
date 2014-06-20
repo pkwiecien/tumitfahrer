e326b4c44ddde854a57c45c05efc457c34b3c502
Pawel Kwiecien <p.kwiecien@tum.de>
2014-06-20 07:53:12 +0200

    updated API

--- app/controllers/api/v2/sessions_controller.rb
+++ app/controllers/api/v2/sessions_controller.rb
@@ -6,19 +6,15 @@ class Api::V2::SessionsController < ApiController
   def create
     # retrieve encrypted credentials
     email, hashed_password = ActionController::HttpAuthentication::Basic::user_name_and_password(request)
-    logger.debug "authenticating user for #{email} and #{hashed_password}"
 
     @user = User.find_by(email: email.downcase)
 
     if @user && @user.authenticate(hashed_password)
-      logger.debug "logging in user #{@user.to_s} with pass digest: #{@user[:password_digest]}"
       if @user.api_key.nil?
         User.generate_api_key(@user)
       end
       respond_with @user, status: :ok
     else
-      logger.debug "could not log in user #{@user.to_s}"
-
       respond_to do |format|
         message = "Can't create session for requested user. Check credentials."
         format.json { render json: {status: :bad_request, message: message} }
