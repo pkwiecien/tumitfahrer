5ba5d35bc5eb08478486b9846f0f981140def431
anu3090 <anuganapath@gmail.com>
2014-06-21 17:17:47 +0200

    Local changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+  before_filter :set_locale
   include SessionsHelper
+
+  def set_locale
+    I18n.locale = extract_locale_from_accept_language_header
+  end
+
+  private
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     hashed_password = Digest::SHA512.hexdigest(params[:session][:password]+'toj369sbz1f316sx')
-    logger.debug "Hashed password: #{hashed_password}, authenticate user: #{user.authenticate(hashed_password)}"
+    #logger.debug "Hashed password: #{hashed_password}, authenticate user: #{user.authenticate(hashed_password)}"
 
     if user && user.authenticate(hashed_password)
       sign_in user
-      redirect_back_or user
+      #redirect_back_or user
+      render :json => { :status => :ok, :message => "Success!" }
     else
       flash.now[:danger] = "Invalid user name or password!"
-      render 'new'
+      render :json => { status: :unprocessable_entity, message: "Invalid email or password!" }
     end
   end
 
