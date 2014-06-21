19187bb7c4beb2cc8273c26ccc55d12d3e016f9a
mShahidAslam <shahid_508@hotmail.com>
2014-06-21 15:02:31 +0200

    added home scree, add ride, sign_in functionality

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,3 +1,4 @@
+require 'geocoder'
 class RidesController < ApplicationController
   before_action :signed_in_user, only: [:new, :destroy]
 
@@ -11,26 +12,34 @@ class RidesController < ApplicationController
   end
 
   def create
-    @ride = current_user.rides.build(ride_params)
+    departure_coordinates = Geocoder.coordinates(params[:ride][:departure_place])
+    destination_coordinates = Geocoder.coordinates(params[:ride][:destination])
+    params[:ride].merge!(:is_driving => params[:is_driving],
+                        :departure_latitude => departure_coordinates[0],
+                        :departure_longitude => departure_coordinates[1],
+                        :destination_latitude => destination_coordinates[0],
+                        :destination_longitude => destination_coordinates[1])
+
+    @ride = Ride.create_ride_by_owner ride_params, current_user
     if @ride.save
 
       # pusher = Grocer.pusher(certificate: "ck.pem", passphrase: 'simina')
-      rideInfo = "Ride from " + @ride.departure_place + " to " + @ride.destination + " on " + @ride.departure_time.to_datetime().strftime('%d %b %Y %H:%M:%S') + " o'clock"
+      #rideInfo = "Ride from " + @ride.departure_place + " to " + @ride.destination + " on " + @ride.departure_time.to_datetime().strftime('%d %b %Y %H:%M:%S') + " o'clock"
       # logger.debug rideInfo
       # note = Grocer::Notification.new(device_token:"f4f382b537d663af6256649e412fc19110cbbdc3d80c04373c090a623810127e", alert: rideInfo,  sound: 'default', badge: 0)
       # pusher.push(note)
 
-      notification = Houston::Notification.new(device: "f4f382b537d663af6256649e412fc19110cbbdc3d80c04373c090a623810127e")
-      notification.alert = rideInfo
-      notification.badge = 57
-      notification.sound = 'default'
-      #APN.push(notification)
-
-      gcm = GCM.new("AIzaSyAOIFGwYitZ12XJu1-DOXuZAa2UaJk97F8")
-      registration_ids= ["APA91bGBYGoCJ5T6HSjW5zZ_tuuc5ZERL5QKYBDl8698O-fLrex9u6L0GtOwupkUvUdLnGSJO_SEtbDYgTqVdLhgdSnTLBo0kQ8h2SvxlCNsVSD8_guyLO4-KNGntJzoA4BXbWRnsCEdXIpwC3tp1_fgUfvdoY69Wg"] # an array of one or more client registration IDs
-      options = {data: {ride: @ride}, collapse_key: "updated_score"}
-      response = gcm.send_notification(registration_ids, options)
-      logger.debug response
+      #notification = Houston::Notification.new(device: "f4f382b537d663af6256649e412fc19110cbbdc3d80c04373c090a623810127e")
+      #notification.alert = rideInfo
+      #notification.badge = 57
+      #notification.sound = 'default'
+      ##APN.push(notification)
+      #
+      #gcm = GCM.new("AIzaSyAOIFGwYitZ12XJu1-DOXuZAa2UaJk97F8")
+      #registration_ids= ["APA91bGBYGoCJ5T6HSjW5zZ_tuuc5ZERL5QKYBDl8698O-fLrex9u6L0GtOwupkUvUdLnGSJO_SEtbDYgTqVdLhgdSnTLBo0kQ8h2SvxlCNsVSD8_guyLO4-KNGntJzoA4BXbWRnsCEdXIpwC3tp1_fgUfvdoY69Wg"] # an array of one or more client registration IDs
+      #options = {data: {ride: @ride}, collapse_key: "updated_score"}
+      #response = gcm.send_notification(registration_ids, options)
+      #logger.debug response
       flash[:success] = "Ride was added!"
       redirect_to current_user
     else
@@ -60,7 +69,9 @@ class RidesController < ApplicationController
   private
 
   def ride_params
-    params.require(:ride).permit(:departure_place, :destination, :departure_time, :free_seats, :meeting_point)
+    params.require(:ride).permit(:departure_place, :destination, :departure_time, :free_seats,
+                                 :meeting_point, :ride_type, :is_driving, :car, :departure_latitude,
+                                 :departure_longitude, :destination_latitude, :destination_longitude)
   end
 
 end
