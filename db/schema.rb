c3f4671611020c2647179039b3931f42e1ed3272
Behroz Sikander <behroz89@gmail.com>
2014-06-22 20:21:33 +0200

    schema file change

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140619072059) do
+ActiveRecord::Schema.define(version: 20140619183844) do
 
   create_table "FriendshipRequests", force: true do |t|
     t.integer  "from_user_id"
